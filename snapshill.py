b989d973df562f5632213fa7b44176bc3caba557
austind9999 <austind9999@hotmail.com>
2019-12-12 19:11:40 -0500

    Update snapshill.py

--- snapshill.py
+++ snapshill.py
@@ -61,6 +61,14 @@ reddit = praw.Reddit(
         user_agent=USER_AGENT,
         )
 
+self.reddit = praw.Reddit(
+    client_id=self.client_id,
+    client_secret=self.client_secret,
+    username=self.username,
+    password=self.password,
+    user_agent=USER_AGENT,
+    )
+
 def get_footer():
     return "\n\n*I am just a simple bot, __not__ a moderator of this subreddit* | [*bot subreddit*]({info}) | [*contact the maintainers*]({contact})".format(
         info=INFO, contact=CONTACT
